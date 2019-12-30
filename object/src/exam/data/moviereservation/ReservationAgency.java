bde32929571231961a7b7a2f3329d8fe58fe2e82
nttejun <nttejun@gmail.com>
2019-12-31 06:52:46 +0900

    Added comment for current code

--- object/src/exam/data/moviereservation/ReservationAgency.java
+++ object/src/exam/data/moviereservation/ReservationAgency.java
@@ -5,6 +5,8 @@ public class ReservationAgency {
         Movie movie = screening.getMovie();
 
         boolean discountable = false;
+        // DiscountCondition 데이터가 변경되면 ReservationAgency 내부 구현도 변경된다 (의존성을 가진 결합상태)
+        // 낮은 응집도 : 할인 정책 추가, 요금 계산방법 추가 등이 발생하는 경우 -> 변경의 이유가 다른 코드들이 하나의 모듈 안에 있어 서로에게 영향이 있다.
         for (DiscountCondition condition : movie.getDiscountConditions()) {
             if (condition.getType() == DiscountConditionType.PERIOD) {
                 discountable = screening.getWhenScreened().getDayOfWeek().equals(condition.getDayOfWeek()) &&
@@ -34,6 +36,9 @@ public class ReservationAgency {
                     break;
             }
 
+            // 아래 코드의 문제점
+            // movie의 getFee 메서드 호출 결과를 Money 타입의 fee로 저장하고 있다
+            // fee 타입이 변경되면 getFee 메서드 호출부인 ReservationAgency 구현도 변경된 타입으로 수정되어야 하는 높은 결합 구조에 있다
             fee = movie.getFee().minus(discountAmount).times(audienceCount);
         } else {
             fee = movie.getFee();
