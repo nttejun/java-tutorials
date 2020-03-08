0aaa443ce4b77619a7a00a0cb0cefeb452abdade
nttejun <nttejun@gmail.com>
2020-03-08 17:50:28 +0900

    Refactoring one and two dimension array

--- educative/src/data/structure/array/TwoDimensionArray.java
+++ educative/src/data/structure/array/TwoDimensionArray.java
@@ -24,7 +24,7 @@ public class TwoDimensionArray {
   }
 
   @Test
-  public void assignLoop() {
+  public void assignByLoop() {
     int[][] twoDimensionArray = new int[10][10];
     for (int i = 0; i < 10; i++) {
       for (int j = 0; j < 10; j++) {
