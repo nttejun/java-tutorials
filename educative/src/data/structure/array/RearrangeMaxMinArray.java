71e509d6201940080fe7d1194847cb0914b8c699
nttejun <nttejun@gmail.com>
2020-03-21 12:21:21 +0900

    Added rearrange max min value array using quick sort

--- educative/src/data/structure/array/RearrangeMaxMinArray.java
+++ educative/src/data/structure/array/RearrangeMaxMinArray.java
@@ -1,24 +1,81 @@
 package data.structure.array;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class RearrangeMaxMinArray {
+
   @Test
-  public void rearrangeMaxMinArray(){
-    int[] arr = {1,2,3,4,5};
+  public void rearrangeMaxMinArray() {
+    int[] arr = {100, 200, 300, 400, 500};
     rearrangeMaxMinArray(arr);
   }
 
   private void rearrangeMaxMinArray(int[] arr) {
     sort(arr, 0, arr.length-1);
+    Assert.assertEquals("500100400200300", printArray(maxMinValue(arr)));
+  }
+
+  private String printArray(int[] arr){
+    String res = "";
+    for(int i : arr){
+      res += i;
+    }
+    return res;
   }
 
   private void sort(int[] arr, int l, int r) {
-    int partition = partition(arr, l, r);
+    int pivotIndex = partition(arr, l, r);
+     if(l < pivotIndex-1){
+       sort(arr, l, pivotIndex-1);
+     }
+    if(pivotIndex < r){
+      sort(arr, pivotIndex, r);
+    }
   }
 
   private int partition(int[] arr, int l, int r) {
+    int pivot = arr[(l + r) / 2];
+    while (l <= r) {
+      while (arr[l] < pivot) {
+        l++;
+      }
+      while (pivot < arr[r]){
+        r--;
+      }
+      if(l <= r){
+        swap(arr, l, r);
+        l++;
+        r--;
+      }
+    }
+      return l;
+  }
 
-    return l;
+  private void swap(int[] arr, int l, int r) {
+    int temp = arr[l];
+    arr[l] = arr[r];
+    arr[r] = temp;
+  }
+
+  private int[] maxMinValue(int[] arr) {
+    int[] result = new int[arr.length];
+    int start = 0;
+    int end = arr.length-1;
+    boolean flag = false;
+
+    for(int i=0; i<arr.length; i++){
+      if(flag == true){
+        result[i] = arr[start];
+        start++;
+        flag = false;
+      } else {
+        result[i] = arr[end];
+        end--;
+        flag = true;
+      }
+    }
+
+    return result;
   }
 }
