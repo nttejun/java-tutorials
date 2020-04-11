a1fc27d194e12879fded7d5a95e66f6ecae8638c
nttejun <nttejun@gmail.com>
2020-04-11 10:14:45 +0900

    Added check balance

--- educative/src/data/structure/stack/CheckBalanceStack.java
+++ educative/src/data/structure/stack/CheckBalanceStack.java
@@ -5,13 +5,47 @@ import org.junit.Test;
 
 public class CheckBalanceStack {
 
-  boolean isBalanced(String expression){
+  boolean isBalanced(String expression) {
 
-    return true;
+    Stack<String> stack = new Stack<>(expression.length());
+
+    for (int i = 0; i < expression.length(); i++) {
+
+      String expressionVal = String.valueOf(expression.charAt(i));
+
+      if (expressionVal.equals("{") || expressionVal.equals("[") || expressionVal.equals("(")) {
+        stack.push(expressionVal);
+
+      } else {
+        switch (expressionVal) {
+          case "}": {
+            if (!stack.pop().equals("{")) {
+              return false;
+            }
+            break;
+          }
+          case "]": {
+            if (!stack.pop().equals("[")) {
+              return false;
+            }
+            break;
+          }
+          case ")": {
+            if (!stack.pop().equals("(")) {
+              return false;
+            }
+            break;
+          }
+        }
+
+      }
+    }
+    if (stack.isEmpty()) return true;
+    return false;
   }
 
   @Test
-  public void isBalanced(){
+  public void isBalanced() {
     Assert.assertEquals(true, isBalanced("{[()]}"));
     Assert.assertEquals(false, isBalanced("[{(}]"));
   }
