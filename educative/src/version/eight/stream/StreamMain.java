2c87c457de883c2af84097530a54f43a73021ace
nttejun <nttejun@gmail.com>
2020-07-17 07:46:35 +0900

    Added list stream and stream of

---
+++ educative/src/version/eight/stream/StreamMain.java
@@ -0,0 +1,28 @@
+package version.eight.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamMain {
+
+  public static void main(String[] args) {
+
+    useStreamOf();
+    useListStream();
+  }
+
+  private static void useListStream() {
+    List<String> list = new ArrayList<>();
+    list.add("a");
+    list.add("b");
+    list.add("c");
+    Stream<String> stream = list.stream();
+    stream.forEach(l -> System.out.println(l));
+  }
+
+  private static void useStreamOf() {
+    Stream<Integer> stream = Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
+    stream.forEach(s -> System.out.println(s));
+  }
+}
