5f694554fc547cbef134cfc313f945d7d13d4592
nttejun <nttejun@gmail.com>
2021-08-01 23:03:19 +0900

    Junit5 테스트 코드 기본 추가

---
+++ junit5/src/test/java/AppTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+  @Test
+  public void create() {
+    AppTest app = new AppTest();
+    assertNotNull(app);
+  }
+}
