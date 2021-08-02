971b356bcee54a79cd713df3623c301eda2b6a00
nttejun <nttejun@gmail.com>
2021-08-02 19:47:01 +0900

    Junit5 기본 어노테이션 코드 추가

--- junit5/src/test/java/AppTest.java
+++ junit5/src/test/java/AppTest.java
@@ -1,5 +1,10 @@
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class AppTest {
@@ -8,5 +13,36 @@ public class AppTest {
   public void create() {
     AppTest app = new AppTest();
     assertNotNull(app);
+  }
+
+  @Test
+  void create1() {
+    System.out.println("create1");
+  }
+
+  @Test
+  @Disabled
+  void disabled() {
+    System.out.println("disabled");
+  }
+
+  @BeforeAll
+  static void beforeAll() {
+    System.out.println("before all");
+  }
+
+  @AfterAll
+  static void afterAll() {
+    System.out.println("after all");
+  }
+
+  @BeforeEach
+  void beforeEach() {
+    System.out.println("before each");
+  }
+
+  @AfterEach
+  void afterEach() {
+    System.out.println("after each");
   }
 }
