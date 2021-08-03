407668aa5e33d5e6b7536255af69d992af5a4085
nttejun <nttejun@gmail.com>
2021-08-03 21:13:43 +0900

    Assertion 기능 (assertAll, assertEquals ramda, assertThrow, assertTimeout) 추가

--- junit5/src/main/java/Study.java
+++ junit5/src/main/java/Study.java
@@ -2,7 +2,20 @@ public class Study {
 
   private StudyStatus status = StudyStatus.DRAFT;
 
+  private int limit = 0;
+
+  public Study(int limit) {
+    if (limit < 0) {
+      throw new IllegalArgumentException("limit은 0보다 커야한다.");
+    }
+    this.limit = limit;
+  }
+
   public StudyStatus getStatus() {
     return this.status;
+  }
+
+  public int getLimit() {
+    return limit;
   }
 }
