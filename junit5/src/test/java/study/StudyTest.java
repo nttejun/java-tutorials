97b012585badfafd82268d99d5dd581b5fe74a62
nttejun <nttejun@gmail.com>
2021-08-08 19:46:50 +0900

    JUnit5 테스트 반복 실행하기 추가

--- junit5/src/test/java/study/StudyTest.java
+++ junit5/src/test/java/study/StudyTest.java
@@ -16,12 +16,38 @@ import java.util.function.Supplier;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.RepetitionInfo;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class StudyTest {
 
   private static Study study = new Study(10);
+
+  /***
+   * @ReoeatedTest : 테스트 반복 실행하기
+   */
+  @DisplayName("스터디 만들기")
+  @RepeatedTest(value = 10, name = "{displayName}, {currentRepetition}/{totalRepetitions}")
+  void repeatTest(RepetitionInfo repetitionInfo) {
+    System.out.println(
+        "Repeat Test : " + repetitionInfo.getCurrentRepetition() + "/" + repetitionInfo
+            .getTotalRepetitions());
+  }
+
+  /***
+   * @ParameterizedTest : 파라미터 값을 받으며 테스트 반복 실행하기
+   */
+  @DisplayName("스터디 만들기")
+  @ParameterizedTest(name = "{index} {displayName} : {0}")
+  @ValueSource(strings = {"날씨가", "많이", "추워지고", "있네요"})
+  void repeatParameterizedTest(String message) {
+    System.out.println(message);
+  }
+
 
   /***
    * 함수 : assertAll()
