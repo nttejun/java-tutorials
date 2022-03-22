90eea7a0e278b9ca15a76d6a54c80d7398168a23
nttejun <nttejun@gmail.com>
2022-03-22 16:42:30 +0900

    [Add] 옵저버 패턴

---
+++ design-pattern/src/observer/StatusEmailSender.java
@@ -0,0 +1,11 @@
+package observer;
+
+public class StatusEmailSender implements StatusObserver {
+    @Override
+    public void onAbnormalStatus(Status status) {
+        sendEmail(status);
+    }
+
+    private void sendEmail(Status status) {
+    }
+}
---
+++ design-pattern/src/observer/StatusObserver.java
@@ -0,0 +1,5 @@
+package observer;
+
+public interface StatusObserver {
+    public void onAbnormalStatus(Status status);
+}
