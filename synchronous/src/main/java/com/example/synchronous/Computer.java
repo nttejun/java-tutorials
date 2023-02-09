0b007c0b675b6f73a862928c58b3ebb121521298
nttejun <nttejun@gmail.com>
2023-02-09 09:54:22 +0900

    [Add] 동기화 프로세스

---
+++ synchronous/src/main/java/com/example/synchronous/Computer.java
@@ -0,0 +1,17 @@
+package com.example.synchronous;
+
+public class Computer {
+
+    public static void main(String[] args) {
+
+        final WebBrowser webBrowser = new WebBrowser(5);
+
+        final Thread threadA = new Thread(new WebSite("Google", webBrowser));
+        final Thread threadB = new Thread(new WebSite("Naver", webBrowser));
+        final Thread threadC = new Thread(new WebSite("Daum", webBrowser));
+
+        threadA.start();
+        threadB.start();
+        threadC.start();
+    }
+}
